462279778539d4f7895080df0cb385d31ecb5f00
stepin104418 <69006493+stepin104418@users.noreply.github.com>
2020-08-13 22:12:26 +0530

    Add files via upload

--- String Seperator/src/stringSeperator.c
+++ String Seperator/src/stringSeperator.c
@@ -1,23 +1,27 @@
 #include "stringSeperator.h"
 #include <string.h>
-#include <stdlib.h>
+#include <stdlib.h> 
 
-struct string_array* stringSeperator(char strInput[])
+const struct string_array* stringSeperator(const char strInput[])
 {
-    char *str;
-    static struct string_array *strOutput;
-    static struct string_array substrings;
+    char *str;                                                   /// Pointer to string array to store input
+    static struct string_array *strOutput;                       /// Pointer to structure(string_array) which will store the returning strings 
+    static struct string_array substrings;                       /// string_array to store the strings 
     strOutput = &substrings;
-    char delimiter[2] = "_";
-    char *token;
-    str = (char *)malloc(((strlen(strInput)+1)* sizeof(char)));
-    strcpy(str, strInput);
-    token = strtok(str, delimiter);
-    int i = 0;
+    memset(strOutput,0,sizeof(substrings));                      ///values of substrings is set to zero to reset any values in the static structure
+    char delimiter[2] = "_";                                     ///character to split strInput[] by
+    char *token;                                                 ///Pointer to implement strtok function
+    str = (char *)malloc(((strlen(strInput)+1)* sizeof(char)));  ///memory is dynamically allocated according to the length of the string
+    strcpy(str, strInput);                                       ///string is copied from strInput to str
+    token = strtok(str, delimiter);                              ///strtok function is implemented from string.h to splint string into tokens
+    int i_tokens = 0;                                            ///iterable
+    /*
+    *Each token is then stored as a character array in the 2D array in substrings
+    */
     while( token != NULL )
     {
-        strcpy(strOutput->string_array[i],token);
-        i++;
+        strcpy(strOutput->string_array[i_tokens],token);
+        i_tokens++;
         token = strtok(NULL, delimiter);
     }
     free(str);
