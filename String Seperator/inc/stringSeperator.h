948c9ad4c3f6245fa0c087ce0c771a199c559f8d
stepin104418 <69006493+stepin104418@users.noreply.github.com>
2020-08-13 15:07:20 +0530

    Update stringSeperator.h

--- String Seperator/inc/stringSeperator.h
+++ String Seperator/inc/stringSeperator.h
@@ -4,7 +4,15 @@
 */
 #ifndef __STRINGSEPERATOR_H__
 #define __STRINGSEPERATOR_H__
-
+/**
+*2D STRING ARRAY
+*A structure is defined to hold a 2D array
+*@param[out] struct string_array consisting of an array[50][50]
+*/
+struct string_array
+{
+    char string_array[50][50];
+};
 /**
 * SEPERATES A STRING
 * @param[in] String which is separated
@@ -12,10 +20,7 @@
 * @return Array of separated sub strings
 * @note Returns -1 for negative values
 */
-struct string_array
-{
-    char string_array[50][50];
-};
+
 struct string_array* stringSeperator(char strInput[]);
 
 #endif /* #ifndef __STRINGSEPERATOR_H__ */
